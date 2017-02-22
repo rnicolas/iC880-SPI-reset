066a77b8844fb8e5b3bfcbc39a689f1ab9f81783
rnicolas <rnicolas@users.noreply.github.com>
2017-02-22 19:36:40 +0100

    Update iC880-SPI_reset.sh

--- iC880-SPI_reset.sh
+++ iC880-SPI_reset.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "17"  &gt; /sys/class/gpio/export
-echo "out" &gt; /sys/class/gpio/gpio17/direction
-echo "1"   &gt; /sys/class/gpio/gpio17/value
+echo "17"  > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio17/direction
+echo "1"   > /sys/class/gpio/gpio17/value
 sleep 5
-echo "0"   &gt; /sys/class/gpio/gpio17/value
+echo "0"   > /sys/class/gpio/gpio17/value
 sleep 1
-echo "0"   &gt; /sys/class/gpio/gpio17/value
+echo "0"   > /sys/class/gpio/gpio17/value
